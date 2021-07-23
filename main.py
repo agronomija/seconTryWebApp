7c0e9eec5f74103320ec50379002ec2de25fcffe
Andraz Corel <serviranje@gmail.com>
2021-07-23 18:38:19 +0200

    login ready

--- main.py
+++ main.py
@@ -7,69 +7,89 @@ app = Flask(__name__)
 db = SQLAlchemy("sqlite:///db.sqlite")
 
 
-class Vislice(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     secret_word = db.Column(db.String, unique=False)
     current_guessed_letter = db.Column(db.String, unique=False)
     guessed_letters = db.Column(db.String, unique=False)
     number_of_tries = db.Column(db.Integer, unique=False)
     missed_tries = db.Column(db.Integer, unique=False)
-    username = db.Column(db.Integer, unique=True)
-
+    user_name = db.Column(db.String, unique=True)
+    user_password = db.Column(db.String, unique=False)
+    user_email = db.Column(db.String, unique=True)
 
 db.create_all()
 
-
-#def get_data_from_db()
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'GET': #poskrbi da če je že nekdo vpisan ga napoti na about_me.html in ga pozdravi,
+        #drugače pa mu napiše da ga ne pozna in mu reče da se mora vpisati ali registrerati..
 
-    user_name = request.cookies.get('user_name')
-    print(user_name)
+        user_name = request.cookies.get('user_name_test')
+        if user_name:
+            print(user_name)
+            return render_template('about_me.html', user_name=user_name)
+        else:
+            print('pod tem imenom ni nobenega cookija..')
+            return render_template('about_me.html')
+
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    piskotek = request.cookies.get('user_name_test')
+
     if request.method == 'GET':
-        return render_template('home.html', user_name=user_name)
-
-    elif request.method == 'POST' and request.form.get('izpis'):
-
-        response = make_response(render_template('home.html', user_name=user_name))
-        response.set_cookie('user_name', expires=0)
-        return response
+        ze_vpisan = 'Nekdo je ze vpisan. Če to niste vi, potem se morate najprej odjaviti'
+        if piskotek:
+            return render_template('about_me.html', ze_vpisan=ze_vpisan)
+        else:
+            nihce_vpisan = 'za nadaljevanje se vpisi..'
+            return render_template('login.html', nihce_vpisan=nihce_vpisan)
 
     elif request.method == 'POST':
-        user_name = request.form.get('user_name')
-        response = make_response(render_template('home.html', user_name=user_name))
-        response.set_cookie('user_name', user_name)
-        return response
-
-@app.route('/about_me')
-def about_me():
-    return render_template('about_me.html')
-
-@app.route('/hangman')
-def hangman():
-    beseda = choose_secret_word('words.txt')
-    user_name = request.cookies.get('user_name')
-    sporocilo = 'Prosim vpišite svoje uporabniško ime za nadaljevanje!'
-    if not user_name:
-        user_name = None
-        return render_template('home.html', sporocilo=sporocilo)
-
-    #igralec = Hangman()
+        password = request.form.get('user-password')
+        email = request.form.get('user-email')
+        user_name = request.form.get('user-name')
 
 
-    return render_template('hangman.html', user_name=user_name)
+        if not db.query(User).filter_by(user_name=user_name).first() and not db.query(User).filter_by(user_email=email).first():
+            user = User(user_name=user_name, user_email=email, user_password=password)
+            response = make_response(render_template('about_me.html', user_name=user_name))
+            response.set_cookie('user_name_test', user_name)
+            return response
+            #tukaj je potrebno dodat cookie
 
-@app.route('/fizz-buzz')
-def fizz_buzz():
-    return render_template('fizz-buzz.html')
+        elif db.query(User).filter_by(user_email=email).first() and db.query(User).filter_by(user_name=user_name).first():
+            enak_username_email = 'Ta email in username že obstajata'
+            return render_template('login.html', enak_username_email=enak_username_email)
 
-@app.route('/tic-tac-toe')
-def tic_tac_toe():
-    return render_template('tic-tac-toe.html')
+        elif db.query(User).filter_by(user_email=email).first():
+            enak_email = 'ta email že obstaja'
+            return render_template('login.html', enak_email=enak_email)
 
+        elif db.query(User).filter_by(user_name=user_name).first():
+            enak_username = 'ta username že obstaja'
+            return render_template('login.html', enak_username=enak_username)
+
+
+
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    return render_template('about_me.html', tukej='prišel iz /signin routa')
 
 
 
 if __name__ == '__main__':
     app.run(use_reloader=True, debug=True)
+
+
+
+
+
+
+
+
+
+
